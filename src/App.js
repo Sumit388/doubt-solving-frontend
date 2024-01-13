c3943a68ffe897d04d4e4f3d1b5f8f801e8edffa
Sumit Mishra <sumit.mishra@uniacco.com>
2024-01-13 20:58:00 +0530

    Fix: Deployment fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // * Packages Import * //
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // * Pages Import * //
 import Homepage from "./pages/Homepage";
@@ -15,28 +15,33 @@ import "react-toastify/dist/ReactToastify.min.css";
 
 function App() {
   const user = getUserDetails();
+
   return (
     <div className="app">
       <Router>
-        <Switch>
-          <Route path="/students/dashboard">
-            {user.userRole === "STUDENT" ? (
-              <StudentDashboard />
-            ) : (
-              <h3>Please Log in to continue.</h3>
-            )}
-          </Route>
-          <Route path="/tutors/dashboard">
-            {user.userRole === "TUTOR" ? (
-              <TutorDashboard />
-            ) : (
-              <h3>Please Log in to continue.</h3>
-            )}
-          </Route>
-          <Route path="/">
-            <Homepage />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route
+            path="/students/dashboard"
+            element={
+              user.userRole === "STUDENT" ? (
+                <StudentDashboard />
+              ) : (
+                <h3>Please Log in to continue.</h3>
+              )
+            }
+          />
+          <Route
+            path="/tutors/dashboard"
+            element={
+              user.userRole === "TUTOR" ? (
+                <TutorDashboard />
+              ) : (
+                <h3>Please Log in to continue.</h3>
+              )
+            }
+          />
+          <Route path="/" element={<Homepage />} />
+        </Routes>
       </Router>
     </div>
   );
